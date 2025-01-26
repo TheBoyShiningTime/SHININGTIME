c93c6a492a8aec5b1669460ec4ad4e6a43718582
ynk02 <namuk0225@naver.com>
2025-01-27 00:03:35 +0900

    Feat: EnemyAI

--- Assets/Scripts/Enemy/EnemyInfo.cs
+++ Assets/Scripts/Enemy/EnemyInfo.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,20 +7,39 @@ public class EnemyInfo : MonoBehaviour
 {
     // Start is called before the first frame update
     
+    Rigidbody2D _rigidbody;
+    
     [Header("Information")]
     [SerializeField] float maxHp;
     [SerializeField] float nowHp;
     [SerializeField] float att;
-    [SerializeField] float speed;
+    public float speed;
+    public float attRange;
+
     
+
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
     void Start()
     {
-        
+        SetEnemyStatus(100,100,10,2);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      
     }
+
+    void SetEnemyStatus(float _hp, float _nowHp, float _att, float _speed)
+    {
+        maxHp = _hp;
+        nowHp = _nowHp;
+        att = _att;
+        speed = _speed;
+    }
+    
 }
